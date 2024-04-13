49bb198e294bdee725adf02661c716a53484d82b
Vlad Florea <vladflorea15@yahoo.ro>
2024-04-13 23:23:55 +0200

    added alias analysis

--- skeleton/TSOConsistencyEnforcer.cpp
+++ skeleton/TSOConsistencyEnforcer.cpp
@@ -6,89 +6,96 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Analysis/LoopInfo.h"
 
 using namespace llvm;
 
 namespace {
 
-class TSOConsistencyEnforcer : public PassInfoMixin<TSOConsistencyEnforcer> {
-public:
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-    bool modified = false;
+    class TSOConsistencyEnforcer : public PassInfoMixin<TSOConsistencyEnforcer> {
+    public:
+        PreservedAnalyses run(Module& M, ModuleAnalysisManager& AM) {
+            bool modified = false;
+            //define function analysis manager (helps to get AA for each function)
+            auto& FAM = AM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
 
-    for (Function &F : M) {
-      if (F.isDeclaration()) continue;
+            for (Function& F : M) {
+                //skip declarations since they don't have instructions
+                if (F.isDeclaration()) continue;
 
-      dbgs() << "Function: " << F.getName() << "\n";
-      for (BasicBlock &BB : F) {
-        for (Instruction &I : BB) {
+                dbgs() << "Function: " << F.getName() << "\n";
 
-          if (isa<LoadInst>(&I) || isa<StoreInst>(&I)) {
-
-            Instruction *NextInst = I.getNextNode();
-            if (NextInst && (isa<LoadInst>(NextInst) || isa<StoreInst>(NextInst))) {
-              if (needsFence(&I, NextInst)) {
-                dbgs() << "Inserting fence between instructions: \n\t"
-                       << I << "\n\t" << *NextInst << "\n";
-                insertMemoryFence(NextInst, modified);
-              } else {
-                dbgs() << "No fence needed between instructions: \n\t"
-                       << I << "\n\t" << *NextInst << "\n";
-              }
+                //define alias analysis for current function
+                auto& AA = FAM.getResult<AAManager>(F);
+                for (BasicBlock& BB : F) {
+                    for (Instruction& I : BB) {
+                        //skip if the instruction is not a load or store
+                        Instruction* NextInst = I.getNextNode();
+                        if (NextInst && (isa<LoadInst>(&I) || isa<StoreInst>(&I)) &&
+                            (isa<LoadInst>(NextInst) || isa<StoreInst>(NextInst))) {
+                            //check if a fence is needed between the two instructions
+                            if (needsFence(&I, NextInst, AA)) {
+                                dbgs() << "Inserting fence between instructions: \n\t"
+                                    << I << "\n\t" << *NextInst << "\n";
+                                insertMemoryFence(NextInst, modified);
+                            }
+                            else {
+                                dbgs() << "No fence needed between instructions: \n\t"
+                                    << I << "\n\t" << *NextInst << "\n";
+                            }
+                        }
+                    }
+                }
             }
-          }
+
+            if (modified) {
+                dbgs() << "Modifications made to module: Fences inserted.\n";
+            }
+            else {
+                dbgs() << "No modifications made to module: No fences inserted.\n";
+            }
+
+            return modified ? PreservedAnalyses::none() : PreservedAnalyses::all();
         }
-      }
-    }
 
-    if (modified) {
-      dbgs() << "Modifications made to module: Fences inserted.\n";
-    } else {
-      dbgs() << "No modifications made to module: No fences inserted.\n";
-    }
+    private:
+        bool needsFence(Instruction* First, Instruction* Second, AAResults& AA) {
+            //Check if the two instructions alias using AA
+            if (AA.alias(MemoryLocation::get(First), MemoryLocation::get(Second)) != AliasResult::NoAlias) {
+                bool isFirstLoad = isa<LoadInst>(First);
+                bool isSecondLoad = isa<LoadInst>(Second);
+                bool isFirstStore = isa<StoreInst>(First);
+                bool isSecondStore = isa<StoreInst>(Second);
+                //Check for potential non-WR reordering
+                if ((isFirstLoad && isSecondStore) || // RW
+                    (isFirstStore && isSecondStore) || // WW
+                    (isFirstLoad && isSecondLoad)) {   // RR
+                    return true;
+                }
+            }
+            return false;
+        }
 
-    return modified ? PreservedAnalyses::none() : PreservedAnalyses::all();
-  }
+        void insertMemoryFence(Instruction* Inst, bool& modified) {
+            //Add a fence after the instruction
+            IRBuilder<> Builder(Inst);
+            auto* Fence = Builder.CreateFence(AtomicOrdering::SequentiallyConsistent, SyncScope::System);
+            modified = true;
 
-private:
-  bool needsFence(Instruction *First, Instruction *Second) {
-    // TSO allows WR without fences; fences are needed for RW, WW, and RR pairs
-    bool isFirstLoad = isa<LoadInst>(First);
-    bool isSecondLoad = isa<LoadInst>(Second);
-    bool isSecondStore = isa<StoreInst>(Second);
-
-    if (isFirstLoad && isSecondStore) {
-      dbgs() << "Detected RW pair.\n";
-    } else if (!isFirstLoad && isSecondStore) {
-      dbgs() << "Detected WW pair.\n";
-    } else if (isFirstLoad && isSecondLoad) {
-      dbgs() << "Detected RR pair.\n";
-    } else {
-      dbgs() << "Detected WR pair (allowed under TSO).\n";
-    }
-
-    return (isFirstLoad && isSecondStore) || (!isFirstLoad && isSecondStore) || (isFirstLoad && isSecondLoad);
-  }
-
-  void insertMemoryFence(Instruction *Inst, bool &modified) {
-    IRBuilder<> Builder(Inst);
-    auto *Fence = Builder.CreateFence(AtomicOrdering::SequentiallyConsistent, SyncScope::System);
-    modified = true;
-
-    dbgs() << "Memory fence inserted: " << *Fence << "\n";
-  }
-};
-
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
-    return {
-        LLVM_PLUGIN_API_VERSION, "MemoryFenceInsertion", "v0.1",
-        [](PassBuilder &PB) {
-            PB.registerPipelineStartEPCallback(
-                [](ModulePassManager &MPM, OptimizationLevel Level) {
-                    MPM.addPass(TSOConsistencyEnforcer());
-                });
+            dbgs() << "Memory fence inserted: " << *Fence << "\n";
         }
     };
-}
+	extern "C" LLVM_ATTRIBUTE_WEAK::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
+		return {
+			LLVM_PLUGIN_API_VERSION, "MemoryFenceInsertion", "v0.1",
+			[](PassBuilder& PB) {
+				PB.registerPipelineStartEPCallback(
+					[](ModulePassManager& MPM, OptimizationLevel Level) {
+						MPM.addPass(TSOConsistencyEnforcer());
+					});
+			}
+		};
+	}
 
 } // namespace
